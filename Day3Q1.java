13456ec6c631a683d2c1dfb76a2a6a552348f521
Saptarshi200 <118371234+Saptarshi200@users.noreply.github.com>
2022-11-16 20:31:49 +0530

    Day 3 Question 1

---
+++ Day3Q1.java
@@ -0,0 +1,15 @@
+public class Day3Q1 {
+    
+    
+    public static void main(String[] args) {
+        int number1 = 5, number2 = 3;
+        int sum = number1 + number2;
+        int substraction = number1 - number2;
+        int multiply = number1 * number2;
+        float divide = (number1 / number2);
+        System.out.println("Sum of the 2 numbers: " + sum);
+        System.out.println("Substraction of the 2 numbers: " + substraction);
+        System.out.println("Multiply of the 2 numbers: " + multiply);
+        System.out.println("Division of the 2 numbers: " + divide);
+    }
+}
