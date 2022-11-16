32eaf6c55ae4e5fa8aa406a1af4bd59dd41783e0
Saptarshi200 <118371234+Saptarshi200@users.noreply.github.com>
2022-11-16 20:42:10 +0530

    Day 3 Question 3

---
+++ Day3Q3.java
@@ -0,0 +1,11 @@
+public class Day3Q3 {
+    
+    
+    public static void main(String[] args) {
+        for(int i=10; i<150; i++) {
+            if(i%23==0) {
+                System.out.println(i);
+            } 
+        }
+    }
+}
